c84e4a9efe4e915e70eba9f2e84339e5d01a572c
Ravi Payasi <ravi.payasi@bonamisoftware.com>
2022-08-23 12:39:11 +0530

    intro to functions

--- intro.js
+++ intro.js
@@ -1,3 +1,5 @@
+// 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
+
 
 // Variables - Variables are of dynamic typed in JS, can be re-assigned to different type
 // datatype
@@ -34,3 +36,71 @@ const d = 5;
 // can't be re-assigned, it will remain undefined
 
 // Both let and const have block scope, hence accessible inside the blocks in which they are defined
+
+
+//////////////////////////////////////////////////////////
+
+// Functions
+// Default return value is undefined for a function
+// A function execution stops as soon as a return statement is found
+// function declaration - 
+function add(a,b){
+    console.log("Function Declaration")
+    return a+b;
+}
+// Paramter - a,b in above function are parameters
+// Arguments - 5,5 passed below are arguments
+const ans1 = add(5,5)   
+// Calling a function and assigning the returned value to a variable
+console.log(ans1)
+
+// function expression
+const func1 = function (a,b){
+
+    return a*b;
+}
+
+const ans2 = func1(2,3)
+console.log(ans2)
+
+// The argument object 
+// Each function contains an object called arguments in it
+// This object behaves as array (although not an array) as we can access it values using array notations
+// arguments[0], arguments[1]
+
+function addArg(){
+    let sum=0;
+    for(let i=0;i<arguments.length;i++){
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+const res1 = addArg(2,3,4,5,6)
+console.log(res1)
+// In above example , even though function definition itself has no parameters, we can still pass
+// arguments while calling the function 
+
+function addArg2(a,b){
+    let summation = 0;
+    for(let i=0;i<arguments.length;i++){
+        summation += arguments[i];
+    }
+    return summation;
+}
+
+const res2 = addArg2(4,5,6,7,8);
+console.log(res2 + " arguemnts with parameters")
+
+// Function Hoisting
+hoisted();
+
+function hoisted(){
+    console.log("I am a hoisted function")
+}
+
+// Above function is first hoisted to top by JS so even though it is defined after it is being called, it will
+// still execute correctly
+
+// Hoisting - JavaScript engine physically moves function declarations to the top of the code before executing them
+
